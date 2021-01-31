4cf38e270481793b5141755714b4ec54a3960dd0
wabbitslayer <wabbitslayer24@gmail.com>
2021-01-31 00:10:05 -0700

    update

--- web/week_3/functions.php
+++ web/week_3/functions.php
@@ -9,7 +9,7 @@ function generateView($results, $images)
 
         //$image =  $_SESSION['image'];
         $view .= '<div>';
-        $view .= "<form action=' index.php?action=addToCart&pId=$item[pId]&title=$item[title]&desc=$item[desc]&price=$item[price]&quan=$item[quan]&image=$item[image]' method=POST>";
+        $view .= "<form action=' ../index.php?action=addToCart&pId=$item[pId]&title=$item[title]&desc=$item[desc]&price=$item[price]&quan=$item[quan]&image=$item[image]' method=POST>";
         //$view .= "<form action='/web/week_3/view/browse.php?pId=$item[pId]&title=$item[title]&desc=$item[desc]&price=$item[price]&quan=$item[quan]&image=$item[image]' method=POST>"; 
         $view .= "<div  class=item name='pId' value=$item[pId]>";
         $view .= "<input type='hidden' name=id value='$item[pId]'>";
