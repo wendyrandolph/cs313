9aab9ae86ab76924374cf06a4ac6bf8352505c9b
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 14:37:54 -0700

    update

--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -47,11 +47,11 @@ try {
 
             <?php
             if ($_SERVER["REQUEST_METHOD"] == "GET" and $text == "") {
-                foreach ($db->query('SELECT category_name, category_id FROM category') as $row) {
+                
 
                     $navList = '<ul>';
-                    $navList .= "<li><a href='index.php' title='View the Recipes Home Page'>Home</a></li>";
-
+                    $navList .= "<li><a href='../recipe/home.php' title='View the Recipes Home Page'>Home</a></li>";
+                    foreach ($db->query('SELECT category_name, category_id FROM category') as $row) {
                     $navList .= "<li><a href='../index.php?action=categories&category_name=" . urlencode($row['category_name']) . "' title='View our $row[category_name] product line'>$row[category_name]</a></li>";
 
                     $navList .= '</ul>';
