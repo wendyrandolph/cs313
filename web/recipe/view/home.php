52162105ce6986eb3fbc143b645325b9147031a9
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 12:40:28 -0700

    update

--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -1,3 +1,33 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+//Query for getting the categories 
+foreach($db->query('SELECT category_name FROM category')as $row){ 
+    echo $row['category_name']; 
+}
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
