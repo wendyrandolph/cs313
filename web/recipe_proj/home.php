b7d9ac740eba37b60e68ee5a2cbd5500f7393958
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-01 22:09:50 -0700

    database connection

--- web/recipe_proj/home.php
+++ web/recipe_proj/home.php
@@ -1,37 +1,41 @@
-<?php 
-session_start(); 
+<?php
+session_start();
 
-  
-    $action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-}
 
-  // Get the database connection file
-  require_once 'library/connections.php';
-  require 'models/main_model.php'; 
+//$action = filter_input(INPUT_POST, 'action');
+//if ($action == NULL) {
+//    $action = filter_input(INPUT_GET, 'action');
+//}
+
+// Get the database connection file
+include 'library/connections.php';
+include 'models/main_model.php';
 
 // Get the array of classifications
 $classifications = getCategories();
 //Get the navigation 
 $getnavigation = navigation($categories);
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RECIPE PROJECT</title>
 </head>
-<body>
-<header> 
-<nav> 
-<?php $getnavigation;  ?> 
-</nav>
-<h1>  THIS IS THE BEGINNING </h1> 
-</header>
-<main> 
 
-</main> 
-    
+<body>
+    <header>
+        <nav>
+            <?php $getnavigation;  ?>
+        </nav>
+        <h1> THIS IS THE BEGINNING </h1>
+    </header>
+    <main>
+
+    </main>
+
 </body>
+
 </html>
