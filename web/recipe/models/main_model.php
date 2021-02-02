05ae7a22ba78b2468399b451b547f30230b92798
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 00:19:29 -0700

    database connection

--- web/recipe/models/main_model.php
+++ web/recipe/models/main_model.php
@@ -5,11 +5,11 @@
 function getCategories($category_name)
 {
     // Create a connection object from the phpmotors connection function
-    
+ 
     // The SQL statement to be used with the database 
-
-    $stmt = $db->prepare('SELECT category_name  FROM category ORDER BY category_name ASC');
-    $stmt->bindValue(':category_name', $category_name, PDO::PARAM_INT);
+    $db =   include '../library/connections.php'; 
+    $stmt = $db->prepare('SELECT category_name  FROM category; ');
+    
 
     $stmt->execute();
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -19,7 +19,6 @@ function getCategories($category_name)
     // stores it as an array in the $classifications variable 
     //$categories = $stmt->fetchAll();
     // The next line closes the interaction with the database 
-    $stmt->closeCursor();
     // The next line sends the array of data back to where the function 
     // was called (this should be the controller) 
 
