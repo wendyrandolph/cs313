fdc5f048850ba8b037b69d5905fd8c8ba2817932
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 00:51:32 -0700

    database connection

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -1,5 +1,23 @@
 <?php 
 
+
+
+$app->get('/db/', function() use($app) {
+    $st = $app['pdo']->prepare('SELECT name FROM test_table');
+    $st->execute();
+  
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+      $app['monolog']->addDebug('Row ' . $row['name']);
+      $names[] = $row;
+    }
+  
+    return $app['twig']->render('database.twig', array(
+      'names' => $names
+    ));
+  });
+
+
 session_start(); 
   
     $action = filter_input(INPUT_POST, 'action');
@@ -7,9 +25,11 @@ if ($action == NULL) {
     $action = filter_input(INPUT_GET, 'action');
 }
 
+
+
 //grab php functions as needed *****************************************************
 
-require '../library/connections.php'; 
+require ' library/connections.php'; 
 //include '../library/functions.php'; 
 //include '../models/main_model.php'; 
 
