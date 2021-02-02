97350d0151589bf1c14538038ff3300c31abfbcd
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 12:57:34 -0700

    update

--- web/recipe/library/connections.php
+++ web/recipe/library/connections.php
@@ -4,7 +4,9 @@ function myDbConnect()
 {
 
 
-  try {
+ 
+try
+{
   $dbUrl = getenv('DATABASE_URL');
 
   $dbOpts = parse_url($dbUrl);
@@ -13,33 +15,17 @@ function myDbConnect()
   $dbPort = $dbOpts["port"];
   $dbUser = $dbOpts["user"];
   $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"], '/');
+  $dbName = ltrim($dbOpts["path"],'/');
 
- 
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    
-    $result = $db->query('SELECT category_name FROM category ORDER BY category_name ASC');
-    // The next line runs the prepared statement 
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetchAll(PDO::FETCH_ASSOC)) {
-        echo "category name: " . $row["category_name"] . "<br>";
-      }
-    } else {
-      echo "0 results";
-    }
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 
-    // The next line gets the data from the database and 
-    // stores it as an array in the $classifications variable 
-    //$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    //return $categories;
-    //if (is_object($db)) {
-    //  echo 'It worked!';
-    //}
-  } catch (PDOException $ex) {
-    echo 'Error!: ' . $ex->getMessage();
-    exit;
-  }
 }
 myDbConnect();
