edfd25028c2c36c4a1506023a245df052644c72d
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 12:51:25 -0700

    update

--- web/recipe/models/main_model.php
+++ web/recipe/models/main_model.php
@@ -4,22 +4,22 @@
 
 function getCategories()
 {
-    
+
     // Create a connection object from the phpmotors connection function
     $db = myDbConnect();
     // The next line creates the prepared statement using the phpmotors connection      
-    
+
     $stmt = $db->prepare('SELECT * FROM category');
-//$stmt->execute(array(':name' => $name, ':id' => $id));
-$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //$stmt->execute(array(':category_name' => $category_name ));
+    $stmt->execute();
+    $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
     // The next line closes the interaction with the database 
-    $stmt->closeCursor();
+   
     // The next line sends the array of data back to where the function 
     // was called (this should be the controller) 
-    
- 
 
 
-    return  $categories; 
-    
+
+
+    return  $categories;
 }
--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -1,34 +1,3 @@
-<?php
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-
-//Query for getting the categories 
-foreach($db->query('SELECT category_name FROM category')as $row){ 
-    echo $row['category_name']; 
-}
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -42,7 +11,9 @@ foreach($db->query('SELECT category_name FROM category')as $row){
 <body>
     <header>
         <nav>
-            <?php echo $categories;  ?>
+
+            <?php if(isset($categories)) { 
+                echo $categories; };  ?>
         </nav>
         <h1> THIS IS THE BEGINNING </h1>
     </header>
