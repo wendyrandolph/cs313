8e6c80ae51a88672239dc403146b982f2a93899c
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-01 19:43:44 -0700

    database connection

--- web/recipe_proj/library/connections.php
+++ web/recipe_proj/library/connections.php
@@ -1,27 +1,26 @@
 <?php 
-
-function myDbConnect() 
+try
 {
   $dbUrl = getenv('DATABASE_URL');
+
   $dbOpts = parse_url($dbUrl);
+
   $dbHost = $dbOpts["host"];
   $dbPort = $dbOpts["port"];
   $dbUser = $dbOpts["user"];
   $dbPassword = $dbOpts["pass"];
   $dbName = ltrim($dbOpts["path"],'/');
 
-  try { 
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-    if(is_object($db)){ 
-        echo 'It worked!'; 
-    }
-
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch (PDOException $ex){
-  echo 'Error!: ' . $ex->getMessage();
-  exit;
+  if (is_object($link)) {
+         echo 'It worked!';
+  }
 }
-} 
-myDbConnect(); 
-?> 
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
