05feae1dc5e9d91268b1b785c7c1f6a3da691c75
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 00:54:03 -0700

    database connection

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -1,24 +1,5 @@
 <?php 
 
-
-
-$app->get('/db/', function() use($app) {
-    $st = $app['pdo']->prepare('SELECT name FROM test_table');
-    $st->execute();
-  
-    $names = array();
-    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-      $app['monolog']->addDebug('Row ' . $row['name']);
-      $names[] = $row;
-    }
-  
-    return $app['twig']->render('database.twig', array(
-      'names' => $names
-    ));
-  });
-
-
-session_start(); 
   
     $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
@@ -29,9 +10,9 @@ if ($action == NULL) {
 
 //grab php functions as needed *****************************************************
 
-require ' library/connections.php'; 
-//include '../library/functions.php'; 
-//include '../models/main_model.php'; 
+include ' library/connections.php'; 
+include ' library/functions.php'; 
+include ' models/main_model.php'; 
 
 
 // Get the array of classifications
