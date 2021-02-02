31cfde3d8d1b099d1c6af0834520dd07dd6efc04
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 14:04:25 -0700

    update

--- web/week_5/scriptures.php
+++ web/week_5/scriptures.php
@@ -70,7 +70,8 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" AND $text == "")
  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
   foreach($rows AS $row)
   {
-    echo '<b>'.'<a href="../week_5/details.php">' .$row['book'].' '.$row['chapter'].':'.$row['verse']. '</a>'. '</b> - 
+      
+    echo '<b>'.'<a href="../week_5/details.php?id=$row[id]">' .$row['book'].' '.$row['chapter'].':'.$row['verse']. '</a>'. '</b> - 
     <input type="hidden" name="id" value="' . $row['id'] . '">'; 
 }}
 
