570a8415ec0119f5c81f43458d205044ab94a165
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 00:03:30 -0700

    database connection

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -15,7 +15,7 @@ include '../models/main_model.php';
 
 
 // Get the array of classifications
-$categories = getCategories();
+$categories = getCategories($category_name);
 //Get the navigation 
 $getnavigation = navigation($categories);
 
