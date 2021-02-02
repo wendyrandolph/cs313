cb9e5018261e39e7c60804be2657bd3f4b4628c5
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-01 23:41:18 -0700

    database connection

--- web/recipe/models/main_model.php
+++ web/recipe/models/main_model.php
@@ -1,34 +1,33 @@
-<?php 
+<?php
 
 //THIS IS THE MAIN MODEL 
 
-function getCategories()
+function getCategories($category_name)
 {
     // Create a connection object from the phpmotors connection function
     $db = myDbConnect();
     // The SQL statement to be used with the database 
-   
-    $sql = $db -> query('SELECT category_name  FROM category ORDER BY category_name ASC');
+
+    $sql = $db->query('SELECT category_name  FROM category ORDER BY category_name ASC');
     // The next line creates the prepared statement using the phpmotors connection      
     $stmt = $db->prepare($sql);
     $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
-$stmt->bindValue(':id', $category_name, PDO::PARAM_INT);
+    $stmt->bindValue(':category_name', $category_name, PDO::PARAM_INT);
 
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    // The next line runs the prepared statement 
     $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // The next line runs the prepared statement 
+   
     // The next line gets the data from the database and 
     // stores it as an array in the $classifications variable 
     //$categories = $stmt->fetchAll();
     // The next line closes the interaction with the database 
-    //$stmt->closeCursor();
+    $stmt->closeCursor();
     // The next line sends the array of data back to where the function 
     // was called (this should be the controller) 
-    
- 
 
 
-    return  $rows; 
-    
+
+
+    return  $rows;
 }
