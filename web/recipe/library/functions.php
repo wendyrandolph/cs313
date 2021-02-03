bebd254a80d0610c15c5fd2fc793192148965f5d
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-03 12:40:28 -0700

    update

--- web/recipe/library/functions.php
+++ web/recipe/library/functions.php
@@ -8,9 +8,9 @@ function getList()
     //make a connection to the database
     $db = myDbConnect();
     // The SQL sttaement to be used with the database
-    $sql = 'SELECT category_name, category_id FROM category';
+   // $sql = 'SELECT category_name, category_id FROM category';
     // The next line creates the prepared statement using the phpmotors connection      
-    $stmt = $db->query($sql);
+    $stmt = $db->query('SELECT category_name, category_id FROM category');
     // The next line runs the prepared statement 
     $stmt->execute();
     // The next line gets the data from the database and 
