2c0400a2859f39892fa6c717e55316d2a6f8ef3e
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 17:20:32 -0700

    update

--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -43,7 +43,7 @@ try {
                 $navList = '<ul>';
                 $navList .= "<li><a href='../view/home.php' title='View the Recipes home page'>Home</a></li><br>";
                 foreach ($db->query('SELECT category_name FROM category') as $row) {
-                    $navList .= '<li>'.'<a href="../view/display.php?action=category&category_id=$row[category_id]">'.' '.$row['category_name'] . ' ' .'</a>'. '</li>'.'<br><br>';
+                    $navList .= '<li>'.'<a href="../view/display.php?category_id=$row[category_id]">'.' '.$row['category_name'] . ' ' .'</a>'. '</li>'.'<br><br>';
                     $navList .= '<input type="hidden" name="category_id" value="' . $row['category_id'] . '" '; 
                 }
                 $navList .= '</ul>';
