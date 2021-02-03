4a05f4a15da62e7b7a9944865be0450d2c454e75
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-02 20:52:55 -0700

    update

--- web/recipe/model/main_model.php
+++ web/recipe/model/main_model.php
@@ -2,6 +2,7 @@
 
 function getCategories($db){ 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $db = myDbConnect(); 
                 $navList = '<ul>';
                 $navList .= "<li><a href='../recipe/index.php' title='View the Recipes home page'>Home</a></li><br><br>";
                 foreach ($db->query('SELECT category_name, category_id FROM category') as $row) {
--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -20,7 +20,7 @@
             <?php
             
                     
-                     echo $buildNav; 
+                    
              ?>
         </nav>
 
