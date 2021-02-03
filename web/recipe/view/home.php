9f3de721f841253e7cec5bf8e3c32eb91920c09c
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-03 14:23:44 -0700

    update

--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -34,7 +34,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/recipe.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../recipe/css/recipe.css" media="screen" />
     <title>RECIPE PROJECT</title>
 </head>
 
