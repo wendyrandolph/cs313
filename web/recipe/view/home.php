b7ddd5db2ab62eafac020a39e55bce4224d3dd86
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-03 14:06:28 -0700

    update

--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -47,7 +47,12 @@ try {
         <nav id="page_nav">
             <?php
             if ($_SERVER["REQUEST_METHOD"] == "GET" and $text == "") {
+                $navList = '<ul>'; 
+                
                 foreach ($db->query('SELECT * FROM category') as $row) {
+
+                    $navList .= "<li><a href='/recipe/?action=display&category_name=" .urlencode($row['category_name']) . "' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";                   
+                    
                     echo '<b>' . $row['category_name'] . ' </b> - <br><br>';
                 }
             } ?>
