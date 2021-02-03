aab2acb3f20c31d6e87e9d361e860adfdd2b24f6
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-03 13:39:22 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -3,11 +3,6 @@
 session_start();
 
 
-//Create the cart with the start of the session if it's not already created. 
-if (!isset($_SESSION['cart'])) {
-    $_SESSION['cart'] = array();
-} //WORKS CORRECTLY 
-
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
@@ -17,9 +12,9 @@ if ($action == NULL) {
 //grab php functions as needed *****************************************************
 require ('../recipe/library/connections.php');
 require ('../recipe/library/functions.php');
-require ('../recipe/model/main_model.php');
+require ('../recipe/library/main_model.php');
 
-//$db = myDbConnect(); 
+
 //$rows = getList($db); 
 
 $navList = getNavigation(); 
