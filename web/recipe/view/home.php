da67e97304d69702e46c3238b9eef62fa1435bdb
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-03 16:28:09 -0700

    update

--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -49,7 +49,7 @@ try {
                 
                 foreach ($db->query('SELECT * FROM category') as $row) {
 
-                    $navList .= "<li><a href='/recipe/?action=default&category_id=$row[category_id]&category_name=" .urlencode($row['category_name']) . "' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";                   
+                    $navList .= "<li><a href='/recipe/?action=display&category_id=$row[category_id]&category_name=" .urlencode($row['category_name']) . "' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";                   
                     
                     $navList .= '</ul>'; 
                    
