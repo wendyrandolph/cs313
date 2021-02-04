fd317d9b1ed9f9011e7b1ef6f0ea5caa82fc30a7
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-04 01:04:58 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -10,48 +10,37 @@ if ($action == NULL) {
 }
 
 //grab php functions as needed *****************************************************
-require ('../recipe/library/connections.php');
-require ('../recipe/library/functions.php');
-require ('../recipe/library/main_model.php');
+require('../recipe/library/connections.php');
+//require ('../recipe/library/functions.php');
+//require ('../recipe/library/main_model.php');
+
+switch ($action) {
+
+    case 'viewRecipe':
+        echo "This is the viewRecipe case statement";
 
 
-//$rows = getList($db); 
+        $index_id = filter_input(INPUT_GET, 'index_id', FILTER_SANITIZE_NUMBER_INT);
+        $recipe_name = filter_input(INPUT_GET, 'recipe_name', FILTER_SANITIZE_STRING);
 
-//$navList = getNavigation(); 
-
-
-
-
-switch($action){ 
+        include '../view/display.php';
+        break;
 
 
-case 'display': 
-//echo "This is the display case statement"; 
+    case 'display':
+        //echo "This is the display case statement"; 
 
-    $category_id = filter_input(INPUT_GET, 'category_id', FILTER_SANITIZE_NUMBER_INT);
+        $category_id = filter_input(INPUT_GET, 'category_id', FILTER_SANITIZE_NUMBER_INT);
         //echo $category_id; 
 
-    include '../recipe/view/home.php';     
-break; 
+        include '../recipe/view/home.php';
+        break;
 
-case 'viewRecipe': 
-echo "This is the viewRecipe case statement"; 
-
-
-$index_id = filter_input(INPUT_GET, 'index_id', FILTER_SANITIZE_NUMBER_INT);
-$recipe_name = filter_input(INPUT_GET, 'recipe_name', FILTER_SANITIZE_STRING); 
+    case 'default':
 
 
 
-    include '../recipe/view/display.php'; 
-    break; 
 
-case 'default': 
-
-    
- 
-
-    include '../recipe/view/home.php'; 
-    break; 
+        include '../recipe/view/home.php';
+        break;
 }
-?>
