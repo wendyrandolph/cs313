5a247ae38381f98185cdb1f0d3ba104b66baa38b
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-04 01:56:44 -0700

    update

--- web/recipe/view/display.php
+++ web/recipe/view/display.php
@@ -1,7 +1,6 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-  $index_id = $_POST['index_id'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $index_id = $_POST['index_id'];
 }
 ?>
 
@@ -23,7 +22,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         <h1> THIS IS THE BEGINNING </h1>
         <nav class="nav">
             <?php
+            if ($_SERVER["REQUEST_METHOD"] == "GET" and $text == "") {
+                $navList = '<ul>';
 
+                foreach ($db->query('SELECT * FROM category') as $row) {
+
+                    $navList .= "<li><a href='/recipe/?action=display&category_id=$row[category_id]&category_name=" . urlencode($row['category_name']) . "' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";
+                }
+                $navList .= '</ul>';
+            }
             echo $navList;
 
             ?>
@@ -32,25 +39,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
     </header>
     <main>
-    <?php 
+        <?php
 
-details($index_id, $db);
+        details($index_id, $db);
 
 
-function details($index_id, $db)
-{
-$stmt = $db->prepare('SELECT recipe_name, recipe, directions FROM ingredients WHERE index_id=:index_id');
-$stmt->bindValue(':index_id', $index_id, PDO::PARAM_INT);
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach($rows AS $row)
-{
-  echo '<b>'.$row['recipe'].' '.$row['recipe'].':'.$row['directions'].'</b> - <br><br>';
-}
+        function details($index_id, $db)
+        {
+            $stmt = $db->prepare('SELECT recipe_name, recipe, directions FROM ingredients WHERE index_id=:index_id');
+            $stmt->bindValue(':index_id', $index_id, PDO::PARAM_INT);
+            $stmt->execute();
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($rows as $row) {
+                echo '<b>' . $row['recipe'] . ' ' . $row['recipe'] . ':' . $row['directions'] . '</b> - <br><br>';
+            }
+        }
 
-}
-
-?>
+        ?>
 
     </main>
 
