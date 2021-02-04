585719d2c459ff7f3e0b0932402877a40a453fe0
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-04 15:22:51 -0700

    update

--- web/recipe/snippets/nav.php
+++ web/recipe/snippets/nav.php
@@ -15,11 +15,11 @@
                 ?>
 
 
-                <div class="container-fluid">
+               
                     <form class="d-flex">
                         <input class="form-control me-2" type="search" placeholder="Disabled for now" aria-label="Search">
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
-                </div>
+               
             </div>
         </nav>
