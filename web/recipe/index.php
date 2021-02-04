dc117b3fdc2b049874c93fd4068dc7f75a7b4315
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-04 01:05:58 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -23,7 +23,7 @@ switch ($action) {
         $index_id = filter_input(INPUT_GET, 'index_id', FILTER_SANITIZE_NUMBER_INT);
         $recipe_name = filter_input(INPUT_GET, 'recipe_name', FILTER_SANITIZE_STRING);
 
-        include '../view/display.php';
+        include '../display.php';
         break;
 
 
