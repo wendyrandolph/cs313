61eb8bfb062a95398fb31115970d59a5052430fa
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-04 13:26:32 -0700

    update

--- web/recipe/view/display.php
+++ web/recipe/view/display.php
@@ -61,7 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt->execute();
             $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             
-                echo $rows['recipe_name']; 
+                var_dump($rows); 
         }
 
         ?>
