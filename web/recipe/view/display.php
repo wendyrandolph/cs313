8edcc4951ee01e344881cc2ab4f133a0b925eba5
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-05 16:55:30 -0700

    update

--- web/recipe/view/display.php
+++ web/recipe/view/display.php
@@ -31,7 +31,7 @@
             <div class="container">
 
                 <div class="amounts">
-
+                    <?php echo $amount ?> 
                 </div>
                 <div class="ingredients">
 
