bec34378620aaeca9696b3af134cd724ec33f44f
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-05 18:42:20 -0700

    update

--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../recipe/css/recipe.css" media="screen" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="../recipe/css/recipe.css" media="screen" />
     <title>RECIPE PROJECT</title>
 </head>
 
@@ -13,15 +14,15 @@
 <body>
     <header>
 
-    <?php include '../recipe/snippets/header.php';
+        <?php include '../recipe/snippets/header.php';
         ?>
-       
-       <nav class="navbar navbar-expand-lg navbar=light bg-light" id="page_nav">
-       <?php include '../recipe/snippets/nav.php';
-        ?>
-       </nav>
+
+        <nav class="navbar navbar-expand-lg navbar=light bg-light" id="page_nav">
+            <?php include '../recipe/snippets/nav.php';
+            ?>
+        </nav>
     </header>
-   
+
     <main>
 
         <?php
@@ -33,9 +34,9 @@
             $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             $results = '<ul>';
-            
+
             foreach ($rows as $row) {
-                $results = "<h4> These are the $row[category_name] recipes.</h4>"; 
+                $results = "<h4> These are the $row[category_name] recipes.</h4>";
                 $results .= "<li class='nav-item'><a href='/recipe/?action=viewRecipe&recipe_name=$row[recipe_name]&recipe_id=$row[recipe_id]'> $row[recipe_name]</a></li>";
             }
             $results .= '</ul>';
