db79f36222ff02ed43b87e8b14be2a39b2036394
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-05 18:39:55 -0700

    update

--- web/recipe/view/display.php
+++ web/recipe/view/display.php
@@ -3,9 +3,11 @@
 
 <head>
     <meta charset="UTF-8">
+   
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../recipe/css/recipe.css" media="screen" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  
+    <link rel="stylesheet" type="text/css" href="../recipe/css/recipe.css" media="screen" />
     <title>RECIPE PROJECT</title>
 </head>
 
