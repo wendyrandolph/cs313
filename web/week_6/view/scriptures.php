78b889843487f6bded23daaf88e96da8f854c4f3
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-09 00:10:17 -0700

    update

--- web/week_6/view/scriptures.php
+++ web/week_6/view/scriptures.php
@@ -20,9 +20,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <h1>Scripture Resources</h1>
 
-    <form method='post' action='../week_6/index.php/?action=details&id=$id' ?> 
+    <form method='post' action='../week_6/index.php/?action=details' ?> 
         <label for="text">Search: </label>
-        <input type="text" id="text" name="text">
+        <input type="text" id="text" name="text" value="id">
     </form>
 
 
