31339ec533a8333119bf358bcb2df9362aeac6b9
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-10 16:57:23 -0700

    registration

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -23,8 +23,8 @@ switch ($action) {
 
     case 'register':
         //Test if I'm getting to the register case. 
-        echo " This is the register case";
-        exit;
+        //   echo " This is the register case";
+        //     exit;
 
         // Filter and store the data
         $member_first_name = filter_input(INPUT_POST, 'member_first_name', FILTER_SANITIZE_STRING);
@@ -32,6 +32,8 @@ switch ($action) {
         $member_email = filter_input(INPUT_POST, 'member_email', FILTER_SANITIZE_EMAIL);
         $member_password = filter_input(INPUT_POST, 'member_password', FILTER_SANITIZE_STRING);
 
+        echo $member_first_name; 
+        exit;
 
         //Checking for an existing email address in the table
         $emailMatch = checkExistingEmail($db, $member_email);
@@ -70,62 +72,62 @@ switch ($action) {
             exit;
         }
 
-        
-
-        case 'Login':
-
-            //filter and store email and password
-            $member_email = filter_input(INPUT_POST, 'member_email', FILTER_SANITIZE_EMAIL);
-            $member_email = checkEmail($member_email);
-            $member_password = filter_input(INPUT_POST, 'member_password', FILTER_SANITIZE_STRING);
-            $checkPassword = checkPassword($member_password);
-            $member_first_name = filter_input(INPUT_POST, 'member_first_name', FILTER_SANITIZE_STRING);
-
-            //Check for empty fields 
-            if (empty($member_email) || empty($checkPassword)) {
-                $_SESSION['message'] = '<p>Please provide information for all empty form fields.</p>';
-                include '../view/login.php';
-                break;
-            }
-
-            // A valid password exists, proceed with the login process
-            // Query the client data based on the email address
-            $clientData = getClient($db, $member_email);
-          
-            // Compare the password just submitted against
-            // the hashed password for the matching client
-            $hashCheck = password_verify($member_password, $clientData['member_password']);
-            // If the hashes don't match create an error
-            // and return to the login view
-            if (!$hashCheck) {
-                $message = '<p class="notice">Please check your password and try again.</p>';
-                include '../view/login.php';
-                break;
-            }
 
 
-            // A valid user exists, log them in
-            $_SESSION['loggedin'] = TRUE;
-            setcookie("firstname", "", "time() -3600", "/");
-            
+    case 'Login':
 
-          
-            // Remove the password from the array
-            // the array_pop function removes the last
-            // element from an array
-            array_pop($clientData);
-            // Store the array into the session
+        //filter and store email and password
+        $member_email = filter_input(INPUT_POST, 'member_email', FILTER_SANITIZE_EMAIL);
+        $member_email = checkEmail($member_email);
+        $member_password = filter_input(INPUT_POST, 'member_password', FILTER_SANITIZE_STRING);
+        $checkPassword = checkPassword($member_password);
+        $member_first_name = filter_input(INPUT_POST, 'member_first_name', FILTER_SANITIZE_STRING);
 
-            $_SESSION['clientData'] = $clientData;
-           
-            $clientId = $_SESSION['clientData']['clientId'];
-           
-        
-      
-            // Send them to the admin view
-            include  '../view/add_recipe.php';
-
+        //Check for empty fields 
+        if (empty($member_email) || empty($checkPassword)) {
+            $_SESSION['message'] = '<p>Please provide information for all empty form fields.</p>';
+            include '../view/login.php';
             break;
+        }
+
+        // A valid password exists, proceed with the login process
+        // Query the client data based on the email address
+        $clientData = getClient($db, $member_email);
+
+        // Compare the password just submitted against
+        // the hashed password for the matching client
+        $hashCheck = password_verify($member_password, $clientData['member_password']);
+        // If the hashes don't match create an error
+        // and return to the login view
+        if (!$hashCheck) {
+            $message = '<p class="notice">Please check your password and try again.</p>';
+            include '../view/login.php';
+            break;
+        }
+
+
+        // A valid user exists, log them in
+        $_SESSION['loggedin'] = TRUE;
+        setcookie("firstname", "", "time() -3600", "/");
+
+
+
+        // Remove the password from the array
+        // the array_pop function removes the last
+        // element from an array
+        array_pop($clientData);
+        // Store the array into the session
+
+        $_SESSION['clientData'] = $clientData;
+
+        $clientId = $_SESSION['clientData']['clientId'];
+
+
+
+        // Send them to the admin view
+        include  '../view/add_recipe.php';
+
+        break;
 
 
     case 'viewRecipe':
@@ -166,11 +168,11 @@ switch ($action) {
         break;
 
 
-        case 'registration':
-        
+    case 'registration':
 
-           include '../recipe/view/registration.php'; 
-           break;  
+
+        include '../recipe/view/registration.php';
+        break;
     default:
 
         include '../recipe/view/login.php';
