0c709398410b557df72bf352f4b489373fe2cb85
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-10 16:59:21 -0700

    registration

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -32,8 +32,7 @@ switch ($action) {
         $member_email = filter_input(INPUT_POST, 'member_email', FILTER_SANITIZE_EMAIL);
         $member_password = filter_input(INPUT_POST, 'member_password', FILTER_SANITIZE_STRING);
 
-        echo $member_first_name; 
-        exit;
+        
 
         //Checking for an existing email address in the table
         $emailMatch = checkExistingEmail($db, $member_email);
