ce1da75ab5b068a2c01872560f83934cffe71b96
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-10 14:51:36 -0700

    registration

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -22,11 +22,11 @@ switch ($action) {
     case 'viewRecipe':
         $recipe_id = filter_input(INPUT_GET, 'recipe_id', FILTER_SANITIZE_NUMBER_INT);
         $recipe_name = filter_input(INPUT_GET, 'recipe_name', FILTER_SANITIZE_STRING);
-        
-       
+
+
         $name = getName($db, $recipe_id);
-        
-        $amount = getIngredients($db, $recipe_id); 
+
+        $amount = getIngredients($db, $recipe_id);
         $recipe = directions($recipe_id, $db);
 
 
@@ -38,29 +38,26 @@ switch ($action) {
         //echo "This is the display case statement"; 
         $recipe_id = filter_input(INPUT_GET, 'recipe_id', FILTER_SANITIZE_NUMBER_INT);
         $category_id = filter_input(INPUT_GET, 'category_id', FILTER_SANITIZE_NUMBER_INT);
-        $recipe_name = filter_input(INPUT_GET, 'recipe_name', FILTER_SANITIZE_STRING); 
+        $recipe_name = filter_input(INPUT_GET, 'recipe_name', FILTER_SANITIZE_STRING);
         //echo $category_id; 
 
-        $results = displayCategory($db, $category_id); 
+        $results = displayCategory($db, $category_id);
 
 
 
         include '../recipe/view/home.php';
         break;
 
-        case 'add_recipe':
-            
-
-
-
-            include '../recipe/view/add_recipe.php';
-            break; 
-
-    case 'default':
+    case 'add_recipe':
 
 
 
 
-        include '../recipe/view/start.php';
+        include '../recipe/view/add_recipe.php';
+        break;
+
+    default:
+
+        include '../recipe/view/login.php';
         break;
 }
