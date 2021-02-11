6711862447a9e3035964ef29d2aab733adbbbbea
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-10 23:17:29 -0700

    registration

--- web/recipe/snippets/header.php
+++ web/recipe/snippets/header.php
@@ -1,3 +1,20 @@
 <h1> Family Recipes </h1>
-        
-        <a href='../recipe/?action=Logout'>Logout</a> 
+    
+
+          
+<?php 
+
+
+if ($_SESSION['loggedin'] === TRUE) {
+
+    //have logged in
+    $loginMessage =  '<a id="my_account" href="/recipe/index.php/?action=logout"> Logout </a>';
+    echo $loginMessage; 
+    echo  '<a id="welcome" href="/phpmotors/accounts/">' . '<h1 class="welcome_message">' . " Welcome " . $_SESSION['clientData']['clientFirstname']. '</h1> </a>';
+} else {
+    $_SESSION['loggedin'] = false;
+    $loginMessage =   '<a id="my_account" href="/recipe/index.php/?action=login"> My Account </a>';
+    echo $loginMessage; 
+}
+?>
+  
