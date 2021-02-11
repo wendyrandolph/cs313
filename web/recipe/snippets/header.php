9f1cf9e928e1f4544cf9f317ea45879a7f15c900
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-10 23:21:36 -0700

    registration

--- web/recipe/snippets/header.php
+++ web/recipe/snippets/header.php
@@ -10,7 +10,7 @@ if ($_SESSION['loggedin'] === TRUE) {
     //have logged in
     $loginMessage =  '<a id="my_account" href="/recipe/?action=logout"> Logout </a>';
     echo $loginMessage; 
-    echo  '<a id="welcome" href="/phpmotors/accounts/">' . '<h1 class="welcome_message">' . " Welcome " . $_SESSION['clientData']['clientFirstname']. '</h1> </a>';
+    echo  '<a id="welcome" href="/phpmotors/accounts/">' . '<h1 class="welcome_message">' . " Welcome " . $_SESSION['clientData']['member_first_name']. '</h1> </a>';
 } else {
     $_SESSION['loggedin'] = false;
     $loginMessage =   '<a id="my_account" href="/recipe/?action=login"> My Account </a>';
