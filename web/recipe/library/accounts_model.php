dc63ecdfd8bf4d2cd0de3fd19e8ef95940c8b8c6
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-10 17:42:44 -0700

    registration

--- web/recipe/library/accounts_model.php
+++ web/recipe/library/accounts_model.php
@@ -17,7 +17,7 @@ function regClient($db, $member_first_name, $member_last_name, $member_email, $h
   $stmt->bindValue(':member_first_name', $member_first_name, PDO::PARAM_STR);
   $stmt->bindValue(':member_last_name', $member_last_name, PDO::PARAM_STR);
   $stmt->bindValue(':member_email', $member_email, PDO::PARAM_STR);
-  $stmt->bindValue(':member_password', $member_password, PDO::PARAM_STR);
+  $stmt->bindValue(':member_password', $hashed_password, PDO::PARAM_STR);
 
   
   // Insert the data
