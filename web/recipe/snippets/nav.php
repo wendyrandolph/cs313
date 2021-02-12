9ea2fb99066be91b0834a4ae5f236f9f324a6f6e
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-11 19:45:13 -0700

    registration

--- web/recipe/snippets/nav.php
+++ web/recipe/snippets/nav.php
@@ -1,15 +1,15 @@
 
             <div class="container-fluid p-2">
-            
-                <?php
-            
-                 $navList = getNav($db); 
-                
-                echo $navList;
-                ?>
+            <?php 
+            $navList = '<ul class="navbar-nav">';
 
+foreach ($db->query('SELECT * FROM category') as $row) {
 
-               
+    $navList .= "<li class='nav-item'><a href='/recipe/?action=display&category_id=$row[category_id]&category_name=" . urlencode($row['category_name']) . "' class='nav-link' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";
+}
+$navList .= '</ul>';
+
+            ?>    
                   
                
             </div>
--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -18,7 +18,7 @@
         ?>
 
         <nav class="navbar navbar-expand-lg navbar=light bg-light" id="page_nav">
-            <?php echo $navList; 
+           <?php include '../recipe/snippets/nav.php';
             ?>
         </nav>
     </header>
