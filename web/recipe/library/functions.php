8724fe19075fa3e6ad227a6caa18dfaae946bc51
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-11 19:56:22 -0700

    registration

--- web/recipe/library/functions.php
+++ web/recipe/library/functions.php
@@ -4,7 +4,7 @@ function getNav($db)
 {
     $navList = '<ul class="navbar-nav">';
 
-    foreach ($db->query('SELECT * FROM category') as $row) {
+    foreach ($db->prepare('SELECT * FROM category') as $row) {
 
         $navList .= "<li class='nav-item'><a href='/recipe/?action=display&category_id=$row[category_id]&category_name=" . urlencode($row['category_name']) . "' class='nav-link' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";
     }
