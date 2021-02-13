d34277c253cdb46ef172c44d57380730cd8e6059
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-13 16:43:26 -0700

    registration

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -166,8 +166,10 @@ switch ($action) {
         include '../recipe/view/home.php';
         break;
     case 'addRecipe':
-        $category_id = $list['category_id']; 
-         $recipe_name = $recipe_desc = $preheat_temp = $cook_time = $instructions = $date_added = $ingredient_name = $required_amount = " ";
+    
+        
+        
+        $category_id = $recipe_name = $recipe_desc = $preheat_temp = $cook_time = $instructions = $date_added = $ingredient_name = $required_amount = " ";
         $category_id = filter_input(INPUT_POST, 'category_id', FILTER_SANITIZE_NUMBER_INT);
         $recipe_name = filter_input(INPUT_POST, 'recipe_name', FILTER_SANITIZE_STRING);
         $recipe_desc  = filter_input(INPUT_POST, 'recipe_desc', FILTER_SANITIZE_STRING);
