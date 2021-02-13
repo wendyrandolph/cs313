81c0c4fe6a6afc53191c1426e9bb1fd3fcddce2f
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-13 15:57:20 -0700

    registration

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -178,7 +178,7 @@ switch ($action) {
         $date_added = filter_input(INPUT_POST, 'date_added', FILTER_SANITIZE_STRING);
         $instructions = filter_input(INPUT_POST, 'instructions', FILTER_SANITIZE_STRING);
 
-        echo $recipe_name, $category_id; 
+       
         // Send the data to the model
         $updateResult = addRecipeName($db, $recipe_name, $recipe_desc, $category_id, $preheat_temp, $cook_time, $date_added, $instructions); 
 
