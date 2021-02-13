afabb9a9d4805c70390672380c3a251552ca9781
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-13 15:48:52 -0700

    registration

--- web/recipe/view/add_recipe.php
+++ web/recipe/view/add_recipe.php
@@ -26,7 +26,7 @@
 
     <main>
 
-    <form action="/recipe/index.php" method="POST">
+    <form action="/recipe/?action=addRecipe" method="POST">
             <label>Which category does your recipe belong to : </label><br>
             <?php echo $list ?> <br><br>
             <input type="hidden" name="category_id" value=" <?php if (isset($list['category_id'])) {
