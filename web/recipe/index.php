352c5cca6d2151e1db380a20eaab1348461a1dd0
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-16 16:01:48 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -176,8 +176,7 @@ switch ($action) {
         $date_added = filter_input(INPUT_POST, 'date_added', FILTER_SANITIZE_STRING);
         $instructions = filter_input(INPUT_POST, 'instructions', FILTER_SANITIZE_STRING);
 
-        $ingredients_name = []; 
-        $required_amounts = []; 
+        
 
         // Send the data to the model
         $updateResult = addRecipeName($db, $recipe_name, $recipe_desc, $category_id, $preheat_temp, $cook_time, $date_added, $instructions, $ingredients_name, $required_amounts);
