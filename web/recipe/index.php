567126bc170ef2b931b09cbece7affebec8223aa
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-16 12:48:16 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -52,11 +52,11 @@ switch ($action) {
 
 
         // // Check for missing data
-        if (empty($member_first_name) || empty($member_last_name) || empty($member_email) || empty($checkPassword)) {
-            $message = '<p>Please provide information for all empty form fields.</p>';
-            include '../recipe/view/registration.php';
-            break;
-        }
+        // if (empty($member_first_name) || empty($member_last_name) || empty($member_email) || empty($checkPassword)) {
+        //     $message = '<p>Please provide information for all empty form fields.</p>';
+        //     include '../recipe/view/registration.php';
+        //     break;
+        // }
         // Send the data to the model
        // $hashed_password = password_hash($member_password, PASSWORD_DEFAULT);
         $regOutcome = regClient($db, $member_first_name, $member_last_name, $member_email, $member_password);
