c520a11d60c8c58f90bafc26c657c624ebcb67dc
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-16 12:37:19 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -65,8 +65,8 @@ switch ($action) {
             break;
         }
         // Send the data to the model
-        $hashed_password = password_hash($member_password, PASSWORD_DEFAULT);
-        $regOutcome = regClient($db, $member_first_name, $member_last_name, $member_email, $hashed_password);
+        //$hashed_password = password_hash($member_password, PASSWORD_DEFAULT);
+        $regOutcome = regClient($db, $member_first_name, $member_last_name, $member_email, $member_password);
 
 
         // Check and report the result
