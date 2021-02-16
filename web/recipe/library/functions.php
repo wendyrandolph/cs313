f9748d640c1e28b636372ad378471fc5280ae1d4
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-16 15:04:25 -0700

    update

--- web/recipe/library/functions.php
+++ web/recipe/library/functions.php
@@ -122,7 +122,7 @@ function displayCategory($db, $category_id)
 }
 
 
-function addRecipeName($db, $recipe_name, $recipe_desc, $category_id, $preheat_temp, $cook_time, $date_added, $instructions, $ingredient_name, $required_amount)
+function addRecipeName($db, $recipe_name, $recipe_desc, $category_id, $preheat_temp, $cook_time, $date_added, $instructions, $ingredients_name, $required_amounts)
 {
     //insert into recipes table 
     $stmt = $db->prepare('INSERT INTO recipes (recipe_name, recipe_desc, category_id, date_added, preheat_temp, cook_time)
@@ -134,8 +134,9 @@ function addRecipeName($db, $recipe_name, $recipe_desc, $category_id, $preheat_t
 
 
     // Insert into ingredients; 
+    if($ingredients_name and $required_amounts)
     $stmt = $db->prepare('INSERT INTO ingredients (ingredient_name, required_amount) VALUES (:ingredient_name, :required_amount)');
-    $stmt->execute(array(':ingredient_name' => $ingredient_name, ':required_amount' => $required_amount));
+    $stmt->execute(array(':ingredient_name' => $ingredients_name, ':required_amount' => $required_amounts));
 
     $newingredientId = $db->lastInsertId(' ingredients_ingredients_id_seq');
 
