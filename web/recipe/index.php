c1a6f604ce35d836f0f633bc98eece3e504b620f
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-17 21:48:33 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -165,7 +165,7 @@ switch ($action) {
         $ingredients_name= array($row['ingredient_name'] => $_POST['ingredient_name'], $row['required_amount'] => $_POST['required_amount']); 
         }
 
-        var_dump($ingredient_name); 
+        var_dump($ingredients_name); 
         
 
         $recipe_name = $recipe_desc = $preheat_temp = $cook_time = $instructions = $date_added = $ingredient_name = $required_amount = " ";
