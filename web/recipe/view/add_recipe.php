27321a996ba95816e4208fc729e7561001243314
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-18 11:15:51 -0700

    update

--- web/recipe/view/add_recipe.php
+++ web/recipe/view/add_recipe.php
@@ -32,7 +32,7 @@
             $data = getCategories($db); 
             $list = " "; 
            foreach($data as $rows){ 
-               $list .= "<input type=radio class='input' name=category_name required> $rows[category_name] " ; 
+               $list .= "<input type=radio class='input' name=category_id required> $rows[category_name] " ; 
                $list.= "<input type=hidden name=category_id value='$rows[category_id]'>"; 
            }
            echo $list; 
