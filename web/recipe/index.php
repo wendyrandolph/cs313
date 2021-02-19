a397064f37b0385fe467040dd5627925184c3e8a
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-18 22:09:57 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -161,25 +161,11 @@ switch ($action) {
         break;
     case 'addRecipe':
 
+        $ingredient_name = $_POST['ingredient_name'];
+        $required_amount = $_POST['required_amount']; 
 
 
-
-        if (is_array($ingredient_name)) {
-            foreach ($ingredient_name as $rows) {
-                $ingredient_name = $_POST['ingredient_name'];
-            }
-        }
-var_dump($ingredient_name); 
-
-        if (is_array($required_amount)) {
-            foreach ($required_amount as $rows) {
-                $required_amount = $_POST['required_amount']; 
-            }
-        }
-var_dump($required_amount); 
-
-
-        $newArray = [$ingredient_name, $required_amount];
+        $newArray = array($ingredient_name, $required_amount);
         foreach ($newArray as $row) {
             $row = array(':ingredient_amount' => $ingredient_name, ':required_amount' => $required_amount);
         }
