047f871fe83612a34adc086abf4f2640149ef264
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-18 18:39:38 -0700

    update

--- web/recipe/library/functions.php
+++ web/recipe/library/functions.php
@@ -147,10 +147,10 @@ function addIngredients($db, $newArray, $required_amount, $category_id)
         $stmt = $db->prepare('INSERT INTO ingredients (ingredient_name, required_amount) VALUES (:ingredient_name, :required_amount)');
         $stmt->execute(array(':ingredient_name' => "$row[ingredient_name]", ':required_amount' => "$row[required_amount]"));
 
-        $newingredientId = $db->lastInsertId(' ingredients_ingredients_id_seq');
+        
     }
-
-
+    $newingredientId = $db->lastInsertId(' ingredients_ingredients_id_seq');
+    
     //insert into recipe_ingredients 
     $stmt = $db->prepare('INSERT INTO recipe_ingredients (ingredients_id, recipe_id, category_id) VALUES (:ingredient_id, :recipe_id, :category_id)');
     $stmt->execute(array(':ingredient_id' => $newingredientId, ':recipe_id' => $newrecipeID, ':category_id' => $category_id));
