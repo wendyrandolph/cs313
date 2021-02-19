8596ed252c01c202527ae8808493a2c84ee1946d
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-18 20:51:59 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -195,7 +195,7 @@ switch ($action) {
 
 
         // Send the data to the model
-        $updateResult = addRecipeName($db, $recipe_name, $recipe_desc, $category_id, $preheat_temp, $cook_time, $date_added, $instructions, $newArray);
+        $updateResult = addRecipeName($db, $recipe_name, $recipe_desc, $category_id, $preheat_temp, $cook_time, $date_added, $instructions, $ingredient_name, $required_amount);
     
 
         if ($addSteps = 1) {
