80fa9e492bbc9a687e167cde4305efda6cf4d45b
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-18 21:47:29 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -164,7 +164,8 @@ switch ($action) {
         $ingredient_name = $_POST['ingredient_name'];
 
         $required_amount = $_POST['required_amount'];
-
+ echo $required_amount; 
+ exit; 
        $newArray = ['ingredient_name' => $ingredient_name, 'required_amount' => $required_amount]; 
         foreach ($newArray as $row) {
             $row = array('ingredient_name' => $ingredient_name, 'required_amount' => $required_amount);
