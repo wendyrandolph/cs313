ee3c920903d2c15ade994078890dfe7676162318
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-19 06:21:16 -0700

    update

--- web/recipe/index.php
+++ web/recipe/index.php
@@ -161,6 +161,11 @@ switch ($action) {
         break;
     case 'addRecipe':
         
+
+        $ingredient_name[] = " "; 
+        $required_amount[] = " "; 
+
+        
         $recipe_name = $recipe_desc = $preheat_temp = $cook_time = $instructions = $date_added =  " ";
         $category_id = filter_input(INPUT_POST, 'category_id', FILTER_SANITIZE_NUMBER_INT);
         $recipe_name = filter_input(INPUT_POST, 'recipe_name', FILTER_SANITIZE_STRING);
