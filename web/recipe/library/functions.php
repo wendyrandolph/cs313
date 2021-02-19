9e07793eb9c6712f97d30196c09fc67f658b7dfa
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-18 23:27:47 -0700

    update

--- web/recipe/library/functions.php
+++ web/recipe/library/functions.php
@@ -152,14 +152,12 @@ function addRecipeName($db, $recipe_name, $recipe_desc, $category_id, $preheat_t
         //var_dump($ingredient_name, $required_amount);
         $newArray = array($ingredients, $amounts);
 
-        var_dump($newArray); 
-        exit; 
         foreach ($newArray as $row) {
             $sql =  'INSERT INTO ingredients (ingredient_name, required_amount) VALUES (:ingredient_name, :required_amount)';
 
             $stmt = $db->prepare($sql);
 
-            $stmt->execute(array(':ingredient_name' => $row[$ingredient_name], ':required_amount' => $row[$required_amount]));
+            $stmt->execute(array(':ingredient_name' => $row[$ingredients], ':required_amount' => $row[$amounts]));
             $newingredientId = $db->lastInsertId('ingredients_ingredients_id_seq');
         }
 
