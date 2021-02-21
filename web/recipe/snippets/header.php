bd41e392a3e037186ce52111191afbf15fe1896c
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 04:20:38 -0700

    update

--- web/recipe/snippets/header.php
+++ web/recipe/snippets/header.php
@@ -14,7 +14,7 @@ if ($_SESSION['loggedin'] === TRUE) {
     echo $myAccount; 
 } else {
     $_SESSION['loggedin'] = false;
-    $loginMessage =   '<a id="my_account" href="/recipe/?action=login"> My Account </a>';
+    $loginMessage =   '<a class="my_account" href="/recipe/?action=login"> My Account </a>';
     echo $loginMessage; 
 }
 ?> 
