5aa54593db211400c8da64d7dd0f5eb4f042b35f
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 15:41:26 -0700

    update

--- web/recipe/library/js/recipe.js
+++ web/recipe/library/js/recipe.js
@@ -1,6 +1,6 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-    var x = document.getElementById("myLinks");
+    var x = document.getElementById("navbar");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
