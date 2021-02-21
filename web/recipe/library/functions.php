d184a1de31b0a063fdb37854ca8acb2827bf2c7f
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 04:43:13 -0700

    update

--- web/recipe/library/functions.php
+++ web/recipe/library/functions.php
@@ -176,13 +176,14 @@ function indexDisplay($list){
 $display = " "; 
     $display = "<table class=table>";
     $display = "<tr><thead> Recipe Name </thead></tr><br />"; 
+    $count = count($list);
     foreach ($list as $row) {
-        $count = count($list); 
+        
         for($i = 1; $i < $count; $i++){ 
 
-        }
-        $display .= "<tr><th scope=row> $i </th> <td>$row[recipe_name] </td> <td> <a href='/recipe/?action=del&recipe_id=$row[recipe_id]' class='rev_delete info'> Delete </a> </td></tr><br />";
         
+        $display .= "<tr><th scope=row> $i </th> <td>$row[recipe_name] </td> <td> <a href='/recipe/?action=del&recipe_id=$row[recipe_id]' class='rev_delete info'> Delete </a> </td></tr><br />";
+        }
     }
     $display .= "</table>";
  return $display; 
