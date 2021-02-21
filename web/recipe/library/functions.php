4a5dfb906b30092ae4d6c8995a3661c8cc610cb6
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 02:53:41 -0700

    update

--- web/recipe/library/functions.php
+++ web/recipe/library/functions.php
@@ -178,7 +178,7 @@ $display = " ";
     $display = "<tr><td> Recipe Name <td>"; 
     foreach ($list as $row) {
 
-        $display .= "<tr><td> $row[recipe_name] </td> <td> <a href='/recipe/?action=del&recipe_id=$row[recipe_id]' class='rev_delete info'> Delete </a> </td></tr>";
+        $display .= "<tr><td> $row[recipe_name] </td> <td> <a href='/recipe/?action=del&recipe_id=$row[recipe_id]' class='rev_delete info'> Delete </a> </td></tr><br />";
         
     }
     $display .= "</table>";
