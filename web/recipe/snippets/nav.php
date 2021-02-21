ecb5c2072dd09913c48952174df594c488e3ea71
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 15:01:40 -0700

    update

--- web/recipe/snippets/nav.php
+++ web/recipe/snippets/nav.php
@@ -1,17 +1,19 @@
-<div class="container-fluid p-2">
+<div class="container-fluid p-2 topnav">
     <?php
-    $navList = '<i class="fas fa-bars hidden"></i> '; 
+    $navList = '<i class="fas fa-bars hidden"></i> ';
     $navList = '<ul class="navbar-nav">';
-    $navList .= "<li class='nav-item'><a href='/recipe/?action=default' class='nav-link' >Home</a></li>";
+    $navList .= "<li class='nav-item'><a href='/recipe/?action=default' class='nav-link active' >Home</a></li>";
     foreach ($db->query('SELECT * FROM category') as $row) {
 
-        $navList .= "<li class='nav-item'><a href='/recipe/?action=display&category_id=$row[category_id]&category_name=" . urlencode($row['category_name']) . "' class='nav-link' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";
+        $navList .= "<li id='my-Links' class='nav-item'><a href='/recipe/?action=display&category_id=$row[category_id]&category_name=" . urlencode($row['category_name']) . "' class='nav-link' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";
     }
     $navList .= '</ul>';
 
     echo $navList;
     ?>
-
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
 
 </div>
 
