777d8dbd0edf62fadb5c78e965efcba1e6e3db84
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 15:14:39 -0700

    update

--- web/recipe/snippets/nav.php
+++ web/recipe/snippets/nav.php
@@ -1,4 +1,4 @@
-
+<!-- 
     <div class="top-nav">
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
             <i class="fa fa-bars"></i>
@@ -16,4 +16,22 @@
         echo $navList;
         ?>
 
-    </div>
+    </div> -->
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- Top Navigation Menu -->
+<div class="topnav">
+  <a href="#home" class="active">Logo</a>
+  <!-- Navigation links (hidden by default) -->
+  <div id="myLinks">
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+  <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
