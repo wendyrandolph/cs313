e377d91ef69937e60af0730c9dbdbec55d4fbc55
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 13:51:44 -0700

    update

--- web/recipe/view/recipe_update.php
+++ web/recipe/view/recipe_update.php
@@ -37,14 +37,13 @@ $_SESSION['loggedin'] = TRUE;
         <div class="card">
             <div class="card-body">
                 <h2> Current Index of Recipes </h2>
-                <div class='edit'>
+                
 
                     <?php
 
                     echo $display; ?>
 
 
-                </div>
 
             </div>
         </div>
