85a74c15c87e64d36c96449eb4e20ba6b2983a5b
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 13:59:14 -0700

    update

--- web/recipe/snippets/nav.php
+++ web/recipe/snippets/nav.php
@@ -1,7 +1,7 @@
 <div class="container-fluid p-2">
     <?php
     $navList = '<ul class="navbar-nav">';
-
+ $navList .= "<li class='nav-item'><a href='/recipe/?action=default'>Home</a></li>"; 
     foreach ($db->query('SELECT * FROM category') as $row) {
 
         $navList .= "<li class='nav-item'><a href='/recipe/?action=display&category_id=$row[category_id]&category_name=" . urlencode($row['category_name']) . "' class='nav-link' title='View our $row[category_name] recipes'>$row[category_name]</a></li>";
