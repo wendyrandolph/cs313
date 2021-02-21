c360cc44e7fd5889549d4fadb4368f8548116305
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 16:04:26 -0700

    update

--- web/recipe/view/home.php
+++ web/recipe/view/home.php
@@ -38,8 +38,8 @@
                     echo " <div class=mx-auto>";
                     echo "<h5> Welcome to your collection of Family Recipe Favorites. Click on a category above to view existing recipes, or login to add new recipes, and delete those you don't love anymore. </h5>";
                 
-                    echo "<img src='../recipe/images/rsz_salsa.jpg' class='rounded .img-thumbnail sm med lg xl'>";
-                    echo "<img src='../recipe/images/rsz_idaho_burrito.jpg' class='rounded .img-thumbnail sm med lg xl'>";
+                    echo "<img src='../recipe/images/rsz_salsa.jpg' class='rounded .img-thumbnail container-fluid'>";
+                    echo "<img src='../recipe/images/rsz_idaho_burrito.jpg' class='rounded .img-thumbnail container-fluid'>";
                     echo "</div>";
                 } else { ?>
                     <h5> These are the <?php $category_name ?> recipes.</h5>
