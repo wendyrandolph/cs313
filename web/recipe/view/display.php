0cada98b050863315422f52f6c4bfcf8ca1508cf
wabbitslayer <wabbitslayer24@gmail.com>
2021-02-21 23:33:14 -0700

    update

---
+++ web/recipe/view/display.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="../recipe/css/recipe.css" media="screen" />
+    <title>RECIPE PROJECT</title>
+</head>
+
+
+<body>
+    <header>
+
+
+        <?php include '../recipe/snippets/header.php';
+        ?>
+
+
+        <nav class="navbar navbar-expand-lg navbar=light bg-light" id="page_nav">
+            <?php include '../recipe/snippets/nav.php';
+            ?>
+        </nav>
+
+    </header>
+    <main>
+
+
+
+        <div class="card">
+            <div class="card-body">
+               
+                    <?php echo $name ?>
+                    <?php echo $amount ?>
+                
+
+
+                <table>
+                    <div class="instructions">
+                        <?php echo $recipe ?>
+                    </div>
+                </table>
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    </main>
+
+</body>
+
+</html>
